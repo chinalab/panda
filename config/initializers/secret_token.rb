c108686f6418b93c0de7734b765c9b29601c1b27
Atsushi Harada <ellegarden1008@gmail.com>
2013-08-25 14:53:50 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Panda::Application.config.secret_key_base = '10ae2af4434cb92f2bd91b3c56d33b22b695a27397be9d59c0fd435c7cc1a74721c3fde83334bf5ccaa9a42ae9609a26415ebb57e9efda9b6d018cdee31a151a'
